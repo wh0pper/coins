498c67b5419d7cdf7909d7d3fc1f6f9acae8e5f7
Jared Clemmensen and russell hofvendahl <russell@>
2018-02-08 09:30:28 -0800

    add tests, methods for dimes, nickels, rounding

--- lib/coins.rb
+++ lib/coins.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
 
 class Coins
+  # new lines in class block?
   def initialize(cents)
-    @cents = cents
+    @cents_remaining = cents.round
   end
 
   def quarters
-    @cents/25  
+    quarters = @cents_remaining/25
+    @cents_remaining %= 25
+    quarters
+  end
+
+  def dimes
+    dimes = @cents_remaining/10
+    @cents_remaining %= 10
+    dimes
+  end
+
+  def nickels
+    nickels = @cents_remaining/5
+    @cents_remaining %= 5
+    nickels
   end
 end
--- spec/coins_spec.rb
+++ spec/coins_spec.rb
@@ -3,16 +3,37 @@ require('coins')
 # require('pry')
 
 # no describe class?
-describe ('#Coins initialize')
-  it ('rounds any float input to integer') do
-    coins = Coins.new(50.5)
-    expect(coins.quarters())to(eq(2))
+describe('Coins') do
+  describe('#initialize') do
+    it ('rounds any float input to integer') do
+      coins = Coins.new(50.5)
+      expect(coins.quarters()).to(eq(2))
+    end
   end
-end
 
-describe ('#quarters') do
-  it ('returns number of quarters in total amount') do
-    coins = Coins.new(75)
-    expect(coins.quarters()).to(eq(3))
+  coins = Coins.new(92)
+  describe('#quarters') do
+    it ('returns number of quarters in total amount') do
+      expect(coins.quarters()).to(eq(3))
+    end
   end
+
+  describe('#dimes') do
+    it ('returns number of dimes in total amount') do
+      expect(coins.dimes()).to(eq(1))
+    end
+  end
+
+  describe('#nickels') do
+    it ('returns number of nickels in total amount') do
+      expect(coins.nickels()).to(eq(1))
+    end
+  end
+
+  # describe('#pennies') do
+  #   it ('returns number of pennies in total amount') do
+  #     coins = Coins.new(75)
+  #     expect(coins.quarters()).to(eq(3))
+  #   end
+  # end
 end
